2b86801d3bd219d931e2b9fe3ad5dcbd687947c1
Mark Peng <markpengisme@gmail.com>
2018-03-02 14:00:59 +0800

    update main

--- TX_touch_price/main.py
+++ TX_touch_price/main.py
@@ -5,15 +5,16 @@ url2="http://info512ah.taifex.com.tw/Future/FusaQuote_Norl.aspx"
 
 def main():
 	price=setting_price()
-	mail_times=0
+	mail_times_max,mail_times=setting_mailtimes()
 	#init
 	url=init_parm()
 	#Every 5 secs check
 	while 1:
 		vartime=time.localtime()
 		url=check_regular_or_after_hour(url,vartime)
-		mail_times=check_price(url,vartime,price,mail_times)
-		time.sleep(0.6)
+		mail_times=check_price(url,vartime,price,mail_times,mail_times_max)
+		time.sleep(1)
+
 
 def init_parm():
 
@@ -28,42 +29,27 @@ def check_regular_or_after_hour(url,vartime):
 	'''
 		Check contract is day or night
 	'''
-	if ((vartime.tm_hour==5 and vartime.tm_min==0 and vartime.tm_sec==5) or 
-		(vartime.tm_hour==13 and vartime.tm_min==45 and vartime.tm_sec==5)):
-		print("睡覺覺時間")
-		#sleep two day if today is Saturday
-		if datetime.date.today().isoweekday()==6:
-			time.sleep(86400*2)		
-
-
-		while(1):
-			if ((time.localtime().tm_hour==8 and time.localtime().tm_min==44 and time.localtime().tm_sec==55) or 
-				(time.localtime().tm_hour==14 and time.localtime().tm_min==59 and time.localtime().tm_sec==55)):
-				print("開盤啦")
-				break
-			time.sleep(0.9)
-			
+		
 	if vartime.tm_hour==8 and vartime.tm_min==45 and vartime.tm_sec==0:
 		url=url1
 	
 	if vartime.tm_hour==15 and vartime.tm_min==0 and vartime.tm_sec==0:	
-		count=1
 		url=url2
 
 	return url
 
-def check_price(url,vartime,price,mail_times):
+def check_price(url,vartime,price,mail_times,mail_times_max):
 	res=requests.post(url)
 	res.encoding='utf-8'
 	df=pandas.read_html(res.text, attrs={'class':'custDataGrid'})[0].iloc[2][2]
 	print(time.strftime('%Y/%m/%d %H:%M:%S',vartime)+'\t即時報價'+df)
 	## 寄信
-	if float(df)<=price[0] and mail_times<=10:
+	if float(df)<=price[0] and mail_times<mail_times_max:
 		print("觸低價")
 		mail.test(str(price[0]))
 		mail_times+=1
 		music.warning_notice()
-	if float(df)>=price[1] and mail_times<=10:
+	if float(df)>=price[1] and mail_times<mail_times_max:
 		print("觸高價")
 		mail.test(str(price[1]))
 		mail_times+=1
@@ -73,13 +59,21 @@ def check_price(url,vartime,price,mail_times):
 def setting_price():
 	##TODO價格相等就寄信
 	while(1):
-		low_price = input("低價位(low price)：")
-		high_price = input("高價位(high price)：")
+		low_price = input("低價位：")
+		high_price = input("高價位：")
 		if low_price.isnumeric() and high_price.isnumeric():
 			price=[float(low_price),float(high_price)]
 			break;
 		print("輸入錯誤，請重新輸入")
 	return price
+
+def setting_mailtimes():
+	while(1):
+		mail_times_max = input("最多寄幾次信：")
+		if mail_times_max.isnumeric() :
+			break;
+		print("輸入錯誤，請重新輸入")
+	return int(mail_times_max),0
 
 
 
