01a842e1c899c3b9a87fda97dabea06845f9c30a
Mark Peng <markpengisme@gmail.com>
2018-04-23 16:43:00 +0800

    complete 三大法人選擇權 crawler

--- TAIFEX_history_data_download/README.md
+++ TAIFEX_history_data_download/README.md
@@ -4,7 +4,7 @@
 ## TODOLIST
 
 - [x]三大法人期貨	http://www.taifex.com.tw/chinese/3/7_12_8.asp
-- [ ]三大法人選擇權 http://www.taifex.com.tw/chinese/3/7_12_10.asp
+- [x]三大法人選擇權 http://www.taifex.com.tw/chinese/3/7_12_10.asp
 - [ ]TX日報	http://www.taifex.com.tw/chinese/3/3_1_2.asp
 - [ ]MTX日報	http://www.taifex.com.tw/chinese/3/3_1_2.asp
 - [ ]TXO日報	http://www.taifex.com.tw/chinese/3/3_2_3.asp
@@ -12,3 +12,4 @@
 ## Example File
 - MXF_三大法人(20150418~20180418).csv
 - TXF_三大法人(20150418~20180418).csv
+- TXO_三大法人(20150423~20180423).csv
--- TAIFEX_history_data_download/main.py
+++ TAIFEX_history_data_download/main.py
@@ -4,7 +4,7 @@ import datetime
 '''
 TODILIST(1):選擇權三大法人
 '''
-def legal_person_futures(futures_type):
+def legal_person(futures_type):
     while(1):
         start = input("請輸入開始年月日(YYYYMMDD):")
         end = input("請輸入結束年月日(YYYYMMDD):")
@@ -23,7 +23,15 @@ def legal_person_futures(futures_type):
         else:
             break
 
-    url="http://www.taifex.com.tw/chinese/3/7_12_8dl.asp"
+    
+    if futures_type=="TXF" or futures_type=="MXF":
+        url="http://www.taifex.com.tw/chinese/3/7_12_8dl.asp"
+    elif futures_type=="TXO":
+        url="http://www.taifex.com.tw/chinese/3/7_12_10dl.asp"
+    else:
+        print('url error')
+        return
+
     #data_test={'syear':'2018','smonth':'4','sday':'18','eyear':'2018','emonth':'4','eday':'18','COMMODITY_ID':'TXF'}
     data={'syear':date_start.year,'smonth':date_start.month,'sday':date_start.day,
            'eyear':date_end.year,'emonth':date_end.month,'eday':date_end.day,
@@ -41,16 +49,19 @@ if __name__ == '__main__':
         print("=================================")
         print("||1.三大法人大台日資料\t\t||")
         print("||2.三大法人小台日資料\t\t||")
+        print("||3.三大法人選擇權日資料\t||")
         print("||0.離開\t\t\t||") 
         print("=================================")
             
         
         feature = input("請輸入需要功能:")
         if feature=='0':
-            exit()
+            break
         elif feature=='1':
-            legal_person_futures("TXF")
+            legal_person("TXF")
         elif feature=='2':
-            legal_person_futures("MXF")
+            legal_person("MXF")
+        elif feature=='3':
+            legal_person("TXO")
         else:
-            print("輸入錯誤，請輸入0~2")
+            print("輸入錯誤，請輸入0~3")
